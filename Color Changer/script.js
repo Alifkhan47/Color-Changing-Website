873eb27f5cd1262dcf8e9523a576375b9c772b96
Alif Khan <49803835+Alifkhan47@users.noreply.github.com>
2023-01-21 23:03:30 +0600

    Add files via upload

---
+++ Color Changer/script.js
@@ -0,0 +1,14 @@
+const button = document.getElementById("color-button");
+
+button.addEventListener("click", function() {
+  document.body.style.backgroundColor = getRandomColor();
+});
+
+function getRandomColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
